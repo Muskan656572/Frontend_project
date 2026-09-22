be3898f46acf0b91bbc40ce8635fcd267aaeafd7
Muskan <muskanbindal825@gmail.com>
2026-09-22 23:50:28 +0530

    doctors-page

---
+++ JS/login.js
@@ -0,0 +1,7 @@
+let btn = document.getElementsByClassName("signup-btn")[0];
+btn.addEventListener("click", function(){
+    alert("Successfully logged in!!");
+    window.location.href="../HTML/home.html";
+}
+)
+
---
+++ JS/signin.js
@@ -0,0 +1,5 @@
+let btn = document.getElementsByClassName("signup-btn")[0];
+btn.addEventListener("click", function(){
+    alert("Successfully signed up!!");
+    window.location.href="../HTML/login.html";
+})
